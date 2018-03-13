52ebfe5c125590b3b6a86ac4df569371b07661de
FabbeX3 <ha.mannen@gmail.com>
2018-03-13 15:09:44 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+// Basic stuff
+const Discord = require("discord.js");
+const bot = new Discord.Client()
+var profanities = require("profanities");
+
+// Login
+client.login(process.env.BOT_TOKEN);
+
+// Listening
+bot.on("message", message =>
+{
+
+    // Variables
+    var sender = message.author;
+    var msg = message.content.toUpperCase();
+    var prefix = "!";
+
+    if(sender.id === "422841374769938433")
+    {
+        return;
+    }
+    // Profanity filter test
+      if(msg === profanities);
+      {
+          const embed =  new Discord.RichEmbed()
+          .setTitle("Warning!!!")
+          .setAuthor("Profanity Bot")
+          .setColor(16711680)
+          .addField(`@${message.author.username} said something suspicious in: #${message.channel.name}`, `The message was: ${message.content}`)
+          .setTimestamp()
+          bot.channels.get("422852036854546432").send({embed});
+      };
+});
